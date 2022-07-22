fdfd0f85a50f1a709dc1ddd31c91c156368b5d17
houakang <houakang@126.com>
2022-07-23 00:39:48 +0800

    fix: 欢迎页调整

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
-import { useIntl, FormattedMessage } from 'umi';
-import styles from './Welcome.less';
-
-const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+import { CheckCircleTwoTone, HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
+import { Card, Space } from 'antd';
 
 const Welcome: React.FC = () => {
   return (
     <PageContainer>
       <Card>
-        wlcome
+      welcome
+      <Space>
+        <SmileTwoTone />
+        <HeartTwoTone twoToneColor="#eb2f96" />
+        <CheckCircleTwoTone twoToneColor="#52c41a" />
+      </Space>
       </Card>
     </PageContainer>
   );
